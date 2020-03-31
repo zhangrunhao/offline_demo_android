1c219ec83542345efb7beedacb33ff9fc503411a
zhangrh <zhangrhweb@163.com>
2020-03-31 16:33:37 +0800

    添加webview

--- app/src/main/java/com/example/offlinedemo/WebViewActivity.java
+++ app/src/main/java/com/example/offlinedemo/WebViewActivity.java
@@ -3,16 +3,37 @@ package com.example.offlinedemo;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class WebViewActivity extends AppCompatActivity {
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    public  void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_view);
+
         WebView myWebView = findViewById(R.id.webview);
         myWebView.loadUrl("http://www.baidu.com");
+        myWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                // 开始加载
+            }
+
+            @Override
+            public  void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                // 加载完成
+            }
+        });
+    }
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+
     }
 }
